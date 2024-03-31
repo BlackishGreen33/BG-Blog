c9388d2d25872f80cff83db9c28c8b67cc1d0dcc
墨綠BG <s5460703@gmail.com>
2024-04-01 01:20:02 +0800

    🐞 fix：修正全局字體樣式未套用的問題

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import Head from 'next/head';
 import { ThemeProvider } from 'next-themes';
 
 import '@/common/styles/globals.scss';
 
 import Layout from '@/common/components/layouts';
-import { firaCode, jakartaSans, soraSans } from '@/common/styles/fonts';
+import GlobalStyles from '@/common/styles/GlobalStyles';
+
+import StyledComponentsRegistry from '../common/libs/registry';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -22,21 +23,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="zh-TW">
-      <Head>
-        <style>
-          {`
-            html {
-              --jakartaSans-font: ${jakartaSans.style.fontFamily};
-              --soraSans-font: ${soraSans.style.fontFamily};
-              --firaCode-font: ${firaCode.style.fontFamily};
-            }
-          `}
-        </style>
-      </Head>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="dark">
-          <Layout>{children}</Layout>
-        </ThemeProvider>
+        <StyledComponentsRegistry>
+          <GlobalStyles />
+          <ThemeProvider attribute="class" defaultTheme="dark">
+            <Layout>{children}</Layout>
+          </ThemeProvider>
+        </StyledComponentsRegistry>
       </body>
     </html>
   );
